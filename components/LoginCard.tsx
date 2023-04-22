39c4c57d1d78323b8d26d64b303a1bbfe1cac9eb
Nikhil Warke <nikhil.warke@artofliving.org>
2023-04-22 06:53:57 +0530

    Update LoginCard

--- components/LoginCard.tsx
+++ components/LoginCard.tsx
@@ -1,51 +1,44 @@
-import * as React from 'react';
+import * as React from 'react'
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Input,
-    Checkbox,
-    Button,
-  } from "@material-tailwind/react";
-   
-  export default function Example() {
-    return (
-      <Card className="w-96">
-        <CardHeader
-          variant="gradient"
-          color="blue"
-          className="mb-4 grid h-28 place-items-center"
-        >
-          <Typography variant="h3" color="white">
-            Sign In
-          </Typography>
-        </CardHeader>
-        <CardBody className="flex flex-col gap-4">
-          <Input label="Email" size="lg" />
-          <Input label="Password" size="lg" />
-          <div className="-ml-2.5">
-            <Checkbox label="Remember Me" />
-          </div>
-        </CardBody>
-        <CardFooter className="pt-0">
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Checkbox,
+  Button
+} from '@material-tailwind/react'
+import Link from 'next/link'
+
+export default function Example() {
+  return (
+    <Card className="w-96">
+      <CardHeader variant="gradient" color="blue" className="mb-4 grid h-28 place-items-center">
+        <Typography variant="h3" color="white">
+          Sign In
+        </Typography>
+      </CardHeader>
+      <CardBody className="flex flex-col gap-4">
+        <Input label="Email" size="lg" />
+        <Input label="Password" size="lg" />
+        <div className="-ml-2.5">
+          <Checkbox label="Remember Me" />
+        </div>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Link href="/teacher-form">
           <Button variant="gradient" fullWidth>
             Sign In
           </Button>
-          <Typography variant="small" className="mt-6 flex justify-center">
-            Don&apos;t have an account?
-            <Typography
-              as="a"
-              href="#signup"
-              variant="small"
-              color="blue"
-              className="ml-1 font-bold"
-            >
-              Sign up
-            </Typography>
+        </Link>
+        <Typography variant="small" className="mt-6 flex justify-center">
+          Don&apos;t have an account?
+          <Typography as="a" href="#signup" variant="small" color="blue" className="ml-1 font-bold">
+            Sign up
           </Typography>
-        </CardFooter>
-      </Card>
-    );
-  }
+        </Typography>
+      </CardFooter>
+    </Card>
+  )
+}
