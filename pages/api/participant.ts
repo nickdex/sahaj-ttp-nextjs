9b610d70be165c1cc2f8185bc20d5c3a3f319d0d
Nikhil Warke <nikhil.warke@artofliving.org>
2023-04-23 19:25:42 +0530

    user can add participant

--- pages/api/participant.ts
+++ pages/api/participant.ts
@@ -1,11 +1,12 @@
-import createClient from 'edgedb';
-import { NextApiRequest, NextApiResponse } from 'next';
+import createClient from 'edgedb'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export const client = createClient();
+export const client = createClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
     const participants = await client.query(
-        `select Participant {
+      `select Participant {
             full_name,
             age,
             id,
@@ -15,5 +16,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             gender,
         };`
     )
-    res.status(200).json(participants);
+    res.status(200).json(participants)
+  } else {
+    const result = await client.execute(
+      `insert Participant { full_name := <str>$full_name, teacher := <str>$teacher, email := <str>$email, gender := <Gender>$gender}`,
+      req.body
+    )
+
+    res.status(201).json(result)
+  }
 }
